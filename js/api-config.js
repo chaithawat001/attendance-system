bb13fef7fbb13de4468acb0c89fbd8070b90ee13
chaithawat001 <chaitvwstom@gmail.com>
2026-03-14 00:46:27 +0700

    Add files via upload

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwSq2fRZZxr6lP-07D9uw_B2chNk48U3UXPzD7zBQioTDCt2glr9oyV7HpcSxwKtQ0/exec';
